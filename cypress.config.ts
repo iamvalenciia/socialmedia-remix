48d3511744b75d5163c2e77c2e45204954eb871a
iamvalenciia <val22018@byui.edu>
2023-03-10 01:44:24 -0500

    fixing cypres config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,27 +2,29 @@ import { defineConfig } from "cypress";
 
 
 export default defineConfig({
-  projectId: '8758eq',
-  e2e: {
-    setupNodeEvents: (on, config) => {
-      const port = "3000";
-      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
-        baseUrl: `http://localhost:${port}`,
-        video: !process.env.CI,
-        screenshotOnRunFailure: !process.env.CI,
-      };
+    projectId: '8758eq',
+    e2e: {
+        setupNodeEvents: (on, config) => {
 
-      // To use this:
-      // cy.task('log', whateverYouWantInTheTerminal)
-      on("task", {
-        log: (message) => {
-          console.log(message);
+            const isDev = config.watchForFileChanges;
+            const port = process.env.PORT ?? (isDev ? "3000" : "8811");
+            const configOverrides: Partial<Cypress.PluginConfigOptions> = {
+                baseUrl: `http://localhost:${port}`,
+                video: !process.env.CI,
+                screenshotOnRunFailure: !process.env.CI,
+            };
 
-          return null;
+            // To use this:
+            // cy.task('log', whateverYouWantInTheTerminal)
+            on("task", {
+                log: (message) => {
+                    console.log(message);
+
+                    return null;
+                },
+            });
+
+            return { ...config, ...configOverrides };
         },
-      });
-
-      return { ...config, ...configOverrides };
     },
-  },
 });
